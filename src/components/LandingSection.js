0523f5f671068f10a39bb42881b6506d62ee01b3
0xjrr <86163146+0xjrr@users.noreply.github.com>
2023-03-30 14:57:49 +0100

    landing section elements

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -5,6 +5,7 @@ import FullScreenSection from "./FullScreenSection";
 const greeting = "Hello, I am Pete!";
 const bio1 = "A frontend developer";
 const bio2 = "specialised in React";
+const avatar = "https://i.pravatar.cc/150?img=7"
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
@@ -15,7 +16,18 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
-
+    <VStack>
+      <Avatar src={avatar} size="medium"/>
+        <Heading size="sm">
+          {greeting}
+        </Heading>
+        <Heading>
+          {bio1}
+        </Heading>
+        <Heading>
+          {bio2}
+        </Heading>
+    </VStack>
   </FullScreenSection>
 );
 
